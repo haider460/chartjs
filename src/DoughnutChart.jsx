f4711e87ebddd538d139822b3602006b9d50589c
haider460 <haiderhassane@gmail.com>
2023-03-07 13:28:01 +0500

    write the function for spaces

--- src/DoughnutChart.jsx
+++ src/DoughnutChart.jsx
@@ -3,14 +3,13 @@ import "chart.js/auto";
 import { FaDownload, FaTimes, FaBars } from "react-icons/fa";
 import { useState, useRef } from "react";
 import { BsBoxArrowUpRight } from "react-icons/bs";
-import ChartDataLabels from "chartjs-plugin-datalabels";
 
 const data = {
   labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
   datasets: [
     {
       label: "My Dataset",
-      data: [12, 19, 3, 5, 2, 3],
+      data: [120, 19, 200, 59, 2, 300],
       backgroundColor: [
         "rgba(255, 99, 132, 1)",
         "rgba(54, 162, 235, 1)",
@@ -38,7 +37,30 @@ data.datasets[0].data.forEach((value) => {
   data.percentageData.push(value);
 });
 
+const giveSpaces = (label, percentage) => {
+  const labelLength = label?.length || 0;
+  const percentageLength = percentage.toString()?.length || 0;
+  const totalLength = 30;
+  const space = " ";
+
+  let returnLabel = "";
+  if (labelLength + percentageLength + 1 > totalLength) {
+    // If the combined length of label and percentage is too long, truncate the label
+    const labelEndIndex = Math.max(totalLength - percentageLength - 4, 0);
+    returnLabel = label.substr(0, labelEndIndex);
+  } else {
+    // Otherwise, pad with spaces
+    const numSpaces = totalLength - labelLength - percentageLength - 1;
+    returnLabel = label + space.repeat(numSpaces) + percentage.toString();
+  }
+
+  console.log(returnLabel);
+
+  return returnLabel;
+};
+
 const options = {
+  cutout: 72,
   plugins: {
     legend: {
       position: "right",
@@ -52,21 +74,10 @@ const options = {
         generateLabels: function (chart) {
           const dataset = chart.data.datasets[0];
           return dataset.data.map((value, index) => {
-            const label =
-              data.labels[index] +
-              "  " +
-              "  " +
-              "  " +
-              "  " +
-              "  " +
-              "  " +
-              "  " +
-              "  " +
-              "  " +
-              "  " +
-              "  " +
-              "  " +
-              data.percentageData[index];
+            const label = giveSpaces(
+              data.labels[index],
+              data.percentageData[index]
+            );
             return {
               text: label,
               fillStyle: dataset.backgroundColor[index],
@@ -87,6 +98,12 @@ const options = {
   },
   color: "#000",
   lineHeight: 1.2,
+  elements: {
+    arc: {
+      borderWidth: 0,
+    },
+  },
+  cutoutPercentage: 72,
 };
 
 const plugins = [
@@ -104,17 +121,17 @@ const plugins = [
         textY = height / 2 - 10;
       const lineHeight = 20;
       text.forEach((line, i) => {
-        ctx.font = fontSize + "em sans-serif";
+        ctx.font = fontSize * 1 + "em sans-serif";
         ctx.textBaseline = "middle";
         if (i === 0) {
           ctx.fillStyle = "black";
         } else {
-          ctx.font = fontSize * 0.8 + "em sans-serif";
+          ctx.font = fontSize * 0.6 + "em sans-serif";
           ctx.fillStyle = "#999";
-          line = " " + line;
+          line = "  " + line;
         }
         const y = textY + i * lineHeight + (i === 0 ? 0 : lineHeight / 2);
-        ctx.fillText(line, textX / 2.3, y);
+        ctx.fillText(line, textX / 2.4, y);
       });
       ctx.save();
     },
@@ -192,8 +209,8 @@ const ChartCard = () => {
                 data={data}
                 options={options}
                 plugins={plugins}
-                height={500}
-                width={500}
+                height={460}
+                width={460}
               />
               <button
                 className="btn btn-sm btn-light d-flex align-items-center text-primary border-primary"
